66644b8eae83f93ecfbebfe51e07375ca7e801cd
Max Raenchuk <raenchuk@yandex-team.ru>
2022-04-03 20:48:27 +0300

    Optimize the algorithm

--- src/backend/seam_carve.py
+++ src/backend/seam_carve.py
@@ -1,31 +1,24 @@
 """Реализация алгоритма seam carving."""
-from typing import Optional
 from typing import Tuple
 
 import numpy as np
 
 
 def horizontal_shrink(
-        image: np.ndarray, derivative: np.ndarray, mask: np.ndarray,
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        image: np.ndarray, derivative: np.ndarray,
+) -> Tuple[np.ndarray, np.ndarray]:
     """Горизонтальное сжатие изображения."""
-    return shrink(image, derivative, mask)
+    return shrink(image, derivative)
 
 
 def vertical_shrink(
-        image: np.ndarray, derivative: np.ndarray, mask: np.ndarray,
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        image: np.ndarray, derivative: np.ndarray,
+) -> Tuple[np.ndarray, np.ndarray]:
     """Вертикальное сжатие изображения."""
-    image, mask, seam = shrink(
-        np.transpose(image, axes=(1, 0, 2)),
-        np.transpose(derivative),
-        np.transpose(mask),
+    image, seam = shrink(
+        np.transpose(image, axes=(1, 0, 2)), np.transpose(derivative),
     )
-    return (
-        np.transpose(image, axes=(1, 0, 2)),
-        np.transpose(mask),
-        np.transpose(seam),
-    )
+    return np.transpose(image, axes=(1, 0, 2)), np.transpose(seam)
 
 
 def compute_seam(derivative: np.ndarray) -> np.ndarray:
@@ -81,24 +74,22 @@ def compute_seam(derivative: np.ndarray) -> np.ndarray:
 
 
 def shrink(
-        image: np.ndarray, derivative: np.ndarray, mask: np.ndarray,
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        image: np.ndarray, derivative: np.ndarray,
+) -> Tuple[np.ndarray, np.ndarray]:
     """Сжатие изображения."""
     seam = compute_seam(derivative)
 
     red = np.ma.compressed(np.ma.MaskedArray(image[:, :, 0], mask=seam))
     green = np.ma.compressed(np.ma.MaskedArray(image[:, :, 1], mask=seam))
     blue = np.ma.compressed(np.ma.MaskedArray(image[:, :, 2], mask=seam))
-    mask = np.ma.compressed(np.ma.MaskedArray(mask, mask=seam))
 
     red = red.reshape(image.shape[0], image.shape[1] - 1)
     green = green.reshape(image.shape[0], image.shape[1] - 1)
     blue = blue.reshape(image.shape[0], image.shape[1] - 1)
-    mask = mask.reshape(image.shape[0], image.shape[1] - 1)
 
     image = np.dstack((red, green, blue))
 
-    return image, mask, seam
+    return image, seam
 
 
 FUNCTIONS = {
@@ -108,16 +99,16 @@ FUNCTIONS = {
 
 
 def seam_carve(
-        image: np.ndarray, action: str, mask: Optional[np.ndarray] = None,
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        image: np.ndarray, action: str,
+) -> Tuple[np.ndarray, np.ndarray]:
     """Основная функция алгоритма seam carving."""
-    if mask is None:
-        mask = np.zeros(image.shape[:-1])
-
-    brightness = (image[:, :, 0] * 0.299 + image[:, :, 1] * 0.587 + image[:, :, 2] * 0.114)
+    brightness = (
+        image[:, :, 0] * 0.299 + image[:, :, 1] * 0.587 + image[:, :, 2] * 0.114
+    )
 
     append_up = np.append(brightness[0][np.newaxis], brightness, axis=0)[:-1]
     append_down = np.append(brightness, brightness[-1][np.newaxis], axis=0)[1:]
+
     append_left = np.append(
         brightness[:, 0][:, np.newaxis], brightness, axis=1,
     )[:, :-1]
@@ -128,6 +119,5 @@ def seam_carve(
     derivative_x = append_right - append_left
     derivative_y = append_down - append_up
     derivative = np.sqrt(derivative_x ** 2 + derivative_y ** 2)
-    derivative += mask.astype('float64') * mask.shape[0] * mask.shape[1] * 256
 
-    return FUNCTIONS[action](image, derivative, mask)
+    return FUNCTIONS[action](image, derivative)
