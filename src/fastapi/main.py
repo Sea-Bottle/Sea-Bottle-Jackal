d019ad3e368eb8d06315261ef47e067f4cfe2302
Tacobella <ilyaem@ulh.ru>
2022-04-14 00:17:59 +0300

    Form processin upload image added

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -1,7 +1,8 @@
 import aiofiles
 from fastapi import FastAPI, File, UploadFile
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from starlette.templating import Jinja2Templates
+from starlette.templating import Jinja2Templates, _TemplateResponse
 from starlette.requests import Request
 
 import os
@@ -12,26 +13,43 @@ from jackalify import jackalify
 
 app = FastAPI()
 templates = Jinja2Templates(directory='src/fastapi/templates')
+app.mount("/static", StaticFiles(directory="src/fastapi/tmp"), name="static")
 
-
-@app.get("/", response_class=HTMLResponse)
-async def main(request: Request):
-    return templates.TemplateResponse('main_form.html', {'request': request})
+picture = None
+video = None
 
 
 @app.post("/", response_class=HTMLResponse)
 async def create_jacklified(
     request: Request, file: UploadFile = File(...)
-):
-    file_name = file.filename
-    async with aiofiles.open(f'src/fastapi/tmp/{file_name}', 'wb') as out_file:
+) -> _TemplateResponse:
+    for file_name in os.listdir('src/fastapi/tmp'):
+        file_path = os.path.join('src/fastapi/tmp', file_name)
+        os.remove(file_path)
+
+    file_name, file_extension = os.path.splitext(file.filename)
+    async with aiofiles.open(f'src/fastapi/tmp/source{file_extension}', 'wb') as out_file:
         content = await file.read()
+        picture = f'source{file_extension}'
         await out_file.write(content)
 
-    jackalify(f'src/fastapi/tmp/{file_name}',
+    jackalify(f'src/fastapi/tmp/source{file_extension}',
               'src/fastapi/tmp/jackalified.mp4')
+    video = 'jackalified.mp4'
 
-    return templates.TemplateResponse('main_form.html', {'request': request})
+    return templates.TemplateResponse('main_form.html',
+                                      {'request': request,
+                                       'picture': picture,
+                                       'video': video})
+
+
+@app.get("/", response_class=HTMLResponse)
+async def show_jackalified(request: Request) -> _TemplateResponse:
+    return templates.TemplateResponse('main_form.html',
+                                      {'request': request,
+                                       'picture': picture,
+                                       'video': video})
+
 
 if __name__ == '__main__':
     import uvicorn
