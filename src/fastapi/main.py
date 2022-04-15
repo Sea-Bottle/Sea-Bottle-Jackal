549219b91c52b527a09e834473b0bad5de097ae6
Tacobella <ilyaem@ulh.ru>
2022-04-15 23:36:11 +0300

    Fix for tests num. 2

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(sys.path[0], '../backend/'))
-from jackalify import jackalify # noqa
+from jackalify import jackalify  # noqa
 
 
 app = FastAPI()
