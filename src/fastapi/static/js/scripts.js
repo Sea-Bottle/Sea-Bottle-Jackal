9027c78a49c144af8e76a5236632911d173b54b0
zykinwases <zykin-00@bk.ru>
2022-04-27 12:37:34 +0300

    script to another file

---
+++ src/fastapi/static/js/scripts.js
@@ -0,0 +1,39 @@
+function checkGif() {
+    setTimeout(() => {
+        fetch("/getGIF", {method: "GET"})
+        .then((res) => {
+            res.json()
+            .then((resp) => {
+                if ((resp.answ) && (resp.path[0])) {
+                    let div = document.getElementById("gif");
+                    let img = document.createElement("img");
+                    img.id = "gifImage"
+                    img.src = "static/working/" + resp.path;
+                    div.style['display'] = 'inherit';
+                    div.prepend(img);
+                } else {
+                    setTimeout(checkGif, 10000);
+                }
+            })
+        })    
+    }, 0)
+}
+
+function checkProgress() {
+    setTimeout(() => {
+        fetch("/getProgress", {method: "GET"})
+        .then((res) => {
+            res.json()
+            .then((resp) => {
+                if (resp.prg)  {
+                    let pb = document.getElementById('progressBar');
+                    pb.value = resp.prg;
+                    if (resp.prg >= 100) {
+                        return;
+                    }
+                }
+                setTimeout(checkProgress, 1000);
+            })
+        })    
+    }, 0)
+}
