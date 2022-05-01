d40289bfb7ab75d6c9d8c3a33b3f40fc300b136c
Zykin Yaroslav <43963580+zykinwases@users.noreply.github.com>
2022-05-01 12:50:50 +0300

    Jackalify progress (#24)
    * progress added
    
    * Dockstring added
    
    * division by 0 check
    
    * progress to ternary
    
    * progress counting into for
    
    * for fixed

--- src/backend/jackalify.py
+++ src/backend/jackalify.py
@@ -13,6 +13,9 @@ from seam_carve import seam_carve
 translation = gettext.translation('src', localedir=os.path.join(os.environ['PROJECT_ROOT'], 'locales'), languages=['en', 'ru'])
 _ = translation.gettext
 
+it = 0
+max_it = 0
+
 
 def jackalify(image_path: str, video_path: str):
     """Apply the seam carving algorithm to the image and get a video with the distortion process.
@@ -22,6 +25,9 @@ def jackalify(image_path: str, video_path: str):
     :param video_path: The path to the output video.
     :type video_path: str
     """
+    global it
+    global max_it
+
     image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
     image = cv2.resize(
         image,
@@ -35,7 +41,8 @@ def jackalify(image_path: str, video_path: str):
 
     frames = []
 
-    for _ in tqdm(range(int(min(height, width) * 0.75))):
+    max_it = int(min(height, width) * 0.75)
+    for it in tqdm(range(max_it)):
         image = seam_carve(image, 'horizontal')
         image = seam_carve(image, 'vertical')
         frames.append(Image.fromarray(np.uint8(cv2.resize(image, (width, height))), mode="RGB"))
@@ -48,6 +55,13 @@ def jackalify(image_path: str, video_path: str):
         duration=25,
         loop=0
     )
+
+
+def getProgress():
+    """Get progress of jackalify."""
+    global it
+    global max_it
+    return (it * 100 // max_it) if max_it > 0 else 0
 
 
 def main():
